0642194a2cdcb7628be651cd0485123c6048368a
MARUF B AWAL <marufbinawal@gmail.com>
2019-05-08 09:12:57 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,11 +1,13 @@
 def addition(num1,num2)
-  puts 5 + 4
+  num1 + num2
 end
 
 def subtraction(num1, num2)
+  num1 - num2
 end
 
 def division(num1, num2)
+  num1 *
 end
 
 def multiplication(num1, num2)
